43a89eb0ad628b380e5e6b9222aadf5b90a75755
danngalann <47415745+danngalann@users.noreply.github.com>
2020-04-13 16:38:09 +0200

    Added recipe component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import { APP_ID, APP_KEY } from "./config";
+import Recipe from "./components/Recipe";
 
 function App() {
-  const getRecipes = async () => {
+  const [recipes, setRecipes] = useState([]);
+  const [recipe, setRecipe] = useState();
+
+  const getRecipes = async (e) => {
+    e.preventDefault();
     fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${recipe}&app_id=${APP_ID}&app_key=${APP_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.hits)
+        setRecipes(data.hits);
       });
   };
 
   return (
-    <div className="App">
-      <form action="" className="searchForm">
-        <input type="text" name="" id="" className="searchBar" />
-        <button type="submit" className="searchBtn" onClick={getRecipes}>
+    <div className="App container">
+      <form onSubmit={getRecipes} className="searchForm">
+        <input
+          type="text"
+          name="recipe"
+          id="recipe-search-bar"
+          className="input-field"
+          onChange={(e) => setRecipe(e.target.value)}
+        />
+        <button type="submit" className="btn">
           Search recipe
         </button>
       </form>
+      <div className="recipes">
+        {recipes.map((hit) => {
+          return <Recipe key={hit.recipe.label} recipe={hit.recipe} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Recipe(props) {
+  
+  const { image, url, label, ingredients } = props.recipe;
+  return (
+    <div className="card blue-grey darken-1">
+      <div className="card-image">
+        <img src={image} alt="" />
+      </div>
+      <div className="card-content white-text">
+        <span className="card-title">{label}</span>
+        {ingredients.map(ingredient => {
+          return <p>{ingredient.text}</p>
+        })}
+      </div>
+      <div className="card-action">
+        <a href={url}>View Recipe</a>
+      </div>
+    </div>
+  );
+}
